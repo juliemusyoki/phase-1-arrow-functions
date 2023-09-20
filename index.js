c3ccbb9c715f3d07a4171cfeb2a4cd1acc09566c
Juliet <musyoki.juliet011@gmail.com>
2023-09-20 21:37:48 +0300

    Done Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    return 2000 / 100;
+  };
+  console.log(divide()); 
+
+  const square = (num) => {
+    return num * num;
+  };
+  const add = (a, b) => {
+    return a + b;
+  };
+  
